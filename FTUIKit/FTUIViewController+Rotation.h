abdfc1ec42e22d5f5f47720f57a672e0c48d8993
Yuichi Fujishige <fujishige@feedtailor.jp>
2012-11-02 16:09:34 +0900

    shouldAutorotateToInterfaceOrientationメソッドからsupportedInterfaceOrientationsマスクを作るカテゴリメソッド追加

---
+++ FTUIKit/FTUIViewController+Rotation.h
@@ -0,0 +1,11 @@
+//
+//  Copyright (c) 2012年 feedtailor Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController (FTUIViewControllerRotation)
+
+- (NSUInteger)ft_supportedInterfaceOrientationsFromShouldAutorotateToInterfaceOrientation;
+
+@end
